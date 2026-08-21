7a525cb8e2039aec455b74c404188fef1598279d
Milos BG <milos.bg.store@gmail.com>
2026-08-21 04:08:40 +0200

    Milos BG

--- app/(root)/components/MilosBGAnimatedSlogan/MilosBGAnimatedSlogan.tsx
+++ app/(root)/components/MilosBGAnimatedSlogan/MilosBGAnimatedSlogan.tsx
@@ -5,17 +5,18 @@ const MilosBGAnimatedSlogan = () => {
   return (
     <section
       aria-label="Milos BG slogan"
-      className="mt-10 flex min-h-[100px] w-full items-center justify-center overflow-hidden bg-[#030303] px-2 py-4 font-kanit sm:min-h-[100px] sm:px-8 lg:min-h-[190px]"
+      className="mt-10 flex min-h-[100px] w-full items-center justify-center bg-[#030303] px-3 py-4 font-kanit sm:min-h-[100px] sm:px-8 lg:min-h-[190px]"
     >
       <h2
         className={`
           ${styles.animatedGradientText}
+          w-full
           whitespace-nowrap
           text-center
-          text-[5vw]
+          text-[4.6vw]
           leading-none
           font-extrabold
-          tracking-[-0.05em]
+          tracking-[-0.055em]
           uppercase
           sm:text-[4.5vw]
           lg:text-[4.6rem]
