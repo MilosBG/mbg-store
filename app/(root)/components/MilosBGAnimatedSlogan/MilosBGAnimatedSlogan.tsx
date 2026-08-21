db0ed338190dddb26129e2c22f40c216ae109e44
Milos BG <milos.bg.store@gmail.com>
2026-08-21 03:52:33 +0200

    Milos BG

--- app/(root)/components/MilosBGAnimatedSlogan/MilosBGAnimatedSlogan.tsx
+++ app/(root)/components/MilosBGAnimatedSlogan/MilosBGAnimatedSlogan.tsx
@@ -8,7 +8,7 @@ const MilosBGAnimatedSlogan = () => {
       className="mt-10 flex min-h-[100px] w-full items-center justify-center overflow-hidden bg-[#030303] px-2 py-4 font-kanit sm:min-h-[100px] sm:px-8 lg:min-h-[190px]"
     >
       <h2
-        className={`${styles.animatedGradientText} text-center text-[clamp(0.46rem,7vw,4.6rem)] leading-[0.95] font-extrabold tracking-[-0.04em] uppercase`}
+        className={`${styles.animatedGradientText} text-center text-[clamp(0.36rem,7vw,4.6rem)] leading-[0.95] font-extrabold tracking-[-0.04em] uppercase`}
       >
         MAKE IT YOUR LIKED OUTFITS
       </h2>
