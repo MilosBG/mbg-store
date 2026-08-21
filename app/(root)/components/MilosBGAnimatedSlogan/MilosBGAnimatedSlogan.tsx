9ddf08f1140d7853c8bd5468f026ba2997170786
Milos BG <milos.bg.store@gmail.com>
2026-08-21 03:44:29 +0200

    Milos BG

--- app/(root)/components/MilosBGAnimatedSlogan/MilosBGAnimatedSlogan.tsx
+++ app/(root)/components/MilosBGAnimatedSlogan/MilosBGAnimatedSlogan.tsx
@@ -5,13 +5,12 @@ const MilosBGAnimatedSlogan = () => {
   return (
     <section
       aria-label="Milos BG slogan"
-      className="mt-10 flex min-h-[280px] w-full items-center justify-center overflow-hidden bg-[#030303] px-2 py-4 font-kanit sm:min-h-[100px] sm:px-8 lg:min-h-[190px]"
+      className="mt-10 flex min-h-[100px] w-full items-center justify-center overflow-hidden bg-[#030303] px-2 py-4 font-kanit sm:min-h-[100px] sm:px-8 lg:min-h-[190px]"
     >
       <h2
-        className={`${styles.animatedGradientText} text-center text-[clamp(0.7rem,7vw,4.6rem)] leading-[0.95] font-extrabold tracking-[-0.04em] uppercase`}
+        className={`${styles.animatedGradientText} text-center text-[clamp(0.46rem,7vw,4.6rem)] leading-[0.95] font-extrabold tracking-[-0.04em] uppercase`}
       >
-        MAKE IT YOUR
-        <br className="sm:hidden" /> LIKED OUTFITS
+        MAKE IT YOUR LIKED OUTFITS
       </h2>
     </section>
   );
