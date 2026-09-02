328d99f107d97d5af03578d7f668cef63940d544
Milos BG <milos.bg.store@gmail.com>
2026-09-02 19:44:38 +0200

    Milos BG ✿

--- app/(root)/the-background/page.tsx
+++ app/(root)/the-background/page.tsx
@@ -24,7 +24,7 @@ export const metadata = buildMetadata({
   keywords: ["brand story", "Milos BG", "basketball culture"],
 });
 
-const VIDEO_ID = "xXFQgvUs08s";
+const VIDEO_ID = "dadrxcC8HOw&t";
 
 const AboutUs = () => {
   return (
