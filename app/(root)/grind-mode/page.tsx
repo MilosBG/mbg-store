80e29a446f1a974491337dba3f8ecc70812af80c
Milos BG <milos.bg.store@gmail.com>
2026-09-19 01:20:28 +0200

    Milos BG ✿

--- app/(root)/grind-mode/page.tsx
+++ app/(root)/grind-mode/page.tsx
@@ -17,15 +17,13 @@ export default async function GrindModePage({
   const playerName = user?.firstName || user?.username || "Hustler";
 
   return (
-   <Container>
-      <div className="mx-auto w-full max-w-[1680px] px-3 pb-10 sm:px-5 xl:px-7 2xl:px-8">
-        <GrindToAchieveClient
-          lang={lang}
-          hustlerName={playerName}
-          bookUrl={process.env.NEXT_PUBLIC_MBG_BOOK_URL || "/the-book"}
-          ebookUrl={process.env.NEXT_PUBLIC_MBG_EBOOK_URL || "/the-book"}
-        />
-      </div>
-   </Container>
+    <Container className="mx-auto w-full max-w-[1680px] px-3 pb-10 sm:px-5 xl:px-7 2xl:px-8">
+      <GrindToAchieveClient
+        lang={lang}
+        hustlerName={playerName}
+        bookUrl={process.env.NEXT_PUBLIC_MBG_BOOK_URL || "/the-book"}
+        ebookUrl={process.env.NEXT_PUBLIC_MBG_EBOOK_URL || "/the-book"}
+      />
+    </Container>
   );
 }
